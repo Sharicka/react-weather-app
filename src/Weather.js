60aed0d4e715455ffab3cd9c8f1f60f011c0195f
Sharicka <sharickassc@gmail.com>
2023-08-23 13:52:49 -0500

    Installed axios then added the API installation

--- src/Weather.js
+++ src/Weather.js
@@ -1,45 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function getResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      city: response.data.name,
+      date: "Thursday 11:50am",
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl:
+        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-6">
+              <input
+                type="search"
+                placeholder="Enter a City"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Current Location" className="btn" />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
         <div className="row">
           <div className="col-6">
-            <input
-              type="search"
-              placeholder="Enter a City"
-              className="form-control"
+            <img
+              src={weatherData.iconUrl}
+              alt={weatherData.description}
+              className="weather-icon"
             />
+            <span className="temp">{Math.round(weatherData.temperature)}</span>
+            <span className="unit">°C</span>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Current Location" className="btn" />
+          <div className="col-6">
+            <ul>
+              <li className="weather-details">
+                Humidity: {weatherData.humidity}%
+              </li>
+              <li className="weather-details">
+                Wind: {Math.round(weatherData.wind)}km/h
+              </li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <h1 className="current-city">Kingston</h1>
-      <ul>
-        <li className="date-time">Thursday 11:10am</li>
-        <li className="temp-description">Clear Sky</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
-            alt="weather icon"
-            className="weather-icon"
-          />
-          <span className="temp">33</span>
-          <span className="unit">°C</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li className="weather-details">Humidity: 20%</li>
-            <li className="weather-details">Wind: 13km/h</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "ca0db41e2e878c74a1dfc7ffece370d4";
+    let city = `Kingston`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(getResponse);
+
+    return "The Weather App is Loading...";
+  }
 }
